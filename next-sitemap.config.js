8ea355bfd85ef04caf31f79aa1536b00e4ff96c8
spencerwozniak <spencerwozniak@gmail.com>
2025-05-18 18:34:38 -0700

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.wozprep.org',
+    siteUrl: 'https://www.spencerwozniak.com',
     generateRobotsTxt: true,
     changefreq: 'weekly',
   };
