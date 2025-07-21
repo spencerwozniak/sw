23e5a611b1331380a1ba62ad72cf46acef9b307d
spencerwozniak <spencerwozniak@gmail.com>
2025-07-20 17:55:46 -0700

    suspense

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import InvoiceClient from './InvoiceClient';
 
 export default function InvoicePage() {
   return (
-  <section className='mt-20'>
-  <InvoiceClient />
-  </section>
+  <Suspense fallback={<div className="flex flex-col justify-center items-center text-center min-h-[90vh] py-10 px-4 sm:px-6 lg:px-8 mt-10">Loading...</div>}>
+    <section className='mt-20'>
+      <InvoiceClient />
+    </section>
+  </Suspense>
   );
 }
